f32a32256e5e67e1f15fb510a56c1ab52518f841
xngar <mantonio.zr@gmail.com>
2025-05-12 14:24:58 -0400

    quitar video

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,12 @@ import Footer from "@/components/Footer";
 import Formulario from "@/components/Formulario";
 
 import Slider from "@/components/Slider";
-import Video from "@/components/Video";
 
 export default function Home() {
   return (
     <div className="">
       <Slider />
-      {/* <Video /> */}
+
       <h2 className="flex justify-center items-center text-[50px] text-[#58167D]">
         ¿ Porque Viajar con American Travel Tour ?
       </h2>
